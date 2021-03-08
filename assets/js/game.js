475a72c7ddcac2135eead4625813e481af2d794d
Katie Redford <katier.@KRedfords-Mini.attlocal.net>
2021-03-07 18:22:44 -0600

    add window prompt and console log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("this is an alert! JavaScript is running!")
+var playerName = window.prompt("What is your robot's name");
+//Not the lack of quotation marks around playerName
+console.log(playerName);
+console.log("this logs a string, good for leaving yourself a message")
+console.log(10+10)
+//what is this
+console.log("Our robot's name is " + playerName)
+
+
+//this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun");
+}
+
+// fight();
